d86288dfdf5b40f605a9d4732a62beffe94c0c2b
Eli Mallon <eli@stream.kitchen>
2017-02-10 15:32:51 -0800

    static

--- src/fcfe.js
+++ src/fcfe.js
@@ -1,6 +1,12 @@
 
+import express from "express";
+import {resolve} from "path";
+
+const app = express();
+
 const ENV = {
   DOMAIN: null,
+  PORT: 80,
 };
 
 Object.keys(ENV).forEach(function(key) {
@@ -11,3 +17,8 @@ Object.keys(ENV).forEach(function(key) {
     throw new Error(`Missing required environment variable ${key}`);
   }
 });
+
+app.use(express.static(resolve(__dirname, "static")));
+
+app.listen(ENV.PORT);
+console.log(`FCFE listening on port ${ENV.PORT}`);
