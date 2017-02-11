0fa781701990c31401367bb715704c0f127911c3
Eli Mallon <eli@stream.kitchen>
2017-02-11 19:27:24 +0000

    failed experiment

--- src/fcfe.js
+++ src/fcfe.js
@@ -2,10 +2,25 @@
 import express from "express";
 import {resolve} from "path";
 import morgan from "morgan";
+import greenlock from "greenlock";
+import standaloneChallenge from "le-challenge-standalone";
+import leStoreCertbot from "le-store-certbot";
+import fs from "fs-promise";
 
+const challenge = standaloneChallenge.create({
+  debug: true
+});
+
+const le = greenlock.create({
+  server: 'staging',
+  challenges: {
+    "http-01": challenge,
+  },
+});
 const app = express();
 
 app.use(morgan("combined"));
+app.use(le.middleware());
 
 const ENV = {
   DOMAIN: null,
@@ -22,6 +37,40 @@ Object.keys(ENV).forEach(function(key) {
 });
 
 app.use(express.static(resolve(__dirname, "static")));
+
+const inProgress = {};
+app.get("/:subdomain", function(req, res) {
+  if (inProgress[subdomain] === true) {
+    return res.status(409).end("This cert is already being issued.");
+  }
+  inProgress[subdomain] = true;
+  const configDir = `/tmp/${Math.round(Math.random() * 10000000000)}`;
+  const le = greenlock.create({
+    server: 'staging',
+    store: leStoreCertbot.create({configDir}),
+    challenges: {
+      "http-01": challenge,
+    },
+  });
+  const subdomain = `${req.params.subdomain}.${ENV.DOMAIN}`;
+  le.register({
+    domains: [subdomain],
+    email: "eli+domains@stream.place",
+    agreeTos: true,
+  })
+  .then(({privkey, cert, chain, subject, altnames, issuedAt, expiresAt}) => {
+    res.json({privkey, cert, chain, subject, altnames, issuedAt, expiresAt});
+    return fs.remove(configDir);
+  })
+  .then((...args) => {
+    inProgress[subdomain] = false;
+  })
+  .catch((err) => {
+    inProgress[subdomain] = false;
+    console.error(err);
+    res.sendStatus(500);
+  });
+});
 
 app.listen(ENV.PORT);
 console.log(`FCFE listening on port ${ENV.PORT}`);
